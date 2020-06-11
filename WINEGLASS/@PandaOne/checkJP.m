ce1727f7e5f4c73b8f6b58401bc59eb18559c7fe
Haopeng <philthinker@sina.com>
2020-06-11 20:20:16 +0800

    Develop ICEWINE on Ubuntu

--- WINEGLASS/@PandaOne/checkJP.m
+++ WINEGLASS/@PandaOne/checkJP.m
@@ -5,7 +5,7 @@ function [flag, error_id] = checkJP(obj, traj)
 %   error_id: integer, indicating the violation item
 %   |   0: No violation
 %   |   1: Joint position violation
-%   |   2: Joint velocity violation
+%   |   2: Joint velocity violation or joint discrepency violation
 %   |   3: Joint acceleration violation
 %   |   4: Joint jerk violation
 %   @PandaOne
@@ -23,18 +23,28 @@ function [flag, error_id] = checkJP(obj, traj)
 
 flag = true;
 error_id = 0;
-dt = 0.001;
+% dt = 0.001;
 
 % q_max, q_min, dq_max, ddq_max, dddq_max
 constraint = obj.JointConstraint;
+% q1, q2, q3, q4, q5, q6, q7
+constraint_deltaJP_empirical = [6e-4, 6e-4, 8e-4, 8e-4, 8e-4, 1e-3, 1e-3];
 
-% traj, dtraj, ddtraj, dddtraj
+% traj
 % Check whether q violates its constraint
 flag = flag && all(max(traj,[],1) < constraint(1,:)) && all(min(traj,[],1) > constraint(2,:));
 if ~flag
     error_id = 1;
     return
 end
+% Check whether q violates the empirical constraint
+dtraj = abs(traj(2:end,:) - traj(1:end-1,:));
+flag = flag && all(max(dtraj,[],1) < constraint_deltaJP_empirical);
+if ~flag
+    error_id = 2;
+    return
+end
+%{
 % Check whether dq violates its constraint
 dtraj = [zeros(1,7); (traj(2:end,:) - traj(1:end-1,:))/dt];
 flag = flag && all(max(abs(dtraj),[],1) < constraint(3,:));
@@ -56,6 +66,7 @@ if ~flag
     error_id = 4;
     return
 end
+%}
 
 end
 
